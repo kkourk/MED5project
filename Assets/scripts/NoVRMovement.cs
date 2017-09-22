0d4041e2e964730ea4be2cf3b04462245e72cb6d
noesmos <tnm@live.dk>
2017-09-22 14:11:08 +0200

    added scene with lift movement

---
+++ Assets/scripts/NoVRMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoVRMovement : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+    float moveX;
+    float moveZ;
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + new Vector3(moveX, 0, moveZ));
+
+        if (transform.parent)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("space key was pressed");
+                transform.parent.GetComponent<platformMovement>().MovePlatform();
+            }
+
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "MovingPlatform")
+        {
+            Debug.Log(other.name);
+            transform.parent = other.transform;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+
+    }
+
+}
---
+++ Assets/scripts/platformMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMovement : MonoBehaviour {
+
+    public GameObject start, end;
+    public float t =0.0f, velocitySpeed = 0.2f;
+
+    float velocity =0;
+    // Use this for initialization
+	void Start () {
+
+        
+}
+	
+	// Update is called once per frame
+	void Update () {
+    t += velocity * Time.deltaTime;
+    if (t > 1.0) { t = 1.0f; }
+    if (t < 0.0) { t = 0.0f; }
+    transform.position = (1 - t) * start.transform.position + t * end.transform.position;
+    }
+
+    public void MovePlatform()
+    {
+        if (transform.position == start.transform.position)
+        {
+            velocity = 0.2f;
+        }
+        else if (transform.position == end.transform.position)
+        {
+            velocity = -0.2f;
+        }
+    }
+}
