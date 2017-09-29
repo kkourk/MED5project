7de4fecb64293c3339c8174ca3687ebc68f247e2
noesmos <tnm@live.dk>
2017-09-29 10:43:47 +0200

    updated the lift scene
    i delete all previous files in our project and put in what we made in the AVALAB

---
+++ Assets/touchbutton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchbutton : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "button")
+        {
+            Debug.Log("debug");
+            GetComponentInParent<platformMovement>().MovePlatform();
+        }
+    }
+}
