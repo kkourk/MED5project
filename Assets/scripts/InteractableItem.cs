bd27f22cb52f949e30ab8ffd8fa4d04ae55f087f
AlexanderSRasmussen <32093781+AlexanderSRasmussen@users.noreply.github.com>
2017-10-03 14:06:30 +0200

    Added code to script

---
+++ Assets/scripts/InteractableItem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public Rigidbody rigidbody;
+
+    private bool currentlyInteracting;
+
+    private float velocityFactor = 20000f;
+    private Vector3 posDelta;
+
+    private float rotationFactor = 600f;
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
+    private Pickupobject attachedPickup;
+
+    private Transform interactionPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        interactionPoint = new GameObject().transform;
+        velocityFactor /= rigidbody.mass;
+        rotationFactor /= rigidbody.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attachedPickup && currentlyInteracting)
+        {
+            posDelta = attachedPickup.transform.position - interactionPoint.position;
+            this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+            rotationDelta = attachedPickup.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+        }
+    }
+
+    public void BeginInteraction(Pickupobject wand)
+    {
+        attachedPickup = wand;
+        interactionPoint.position = wand.transform.position;
+        interactionPoint.rotation = wand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+
+    public void EndInteraction(Pickupobject wand)
+    {
+        if (wand == attachedPickup)
+        {
+            attachedPickup = null;
+            currentlyInteracting = false;
+        }
+    }
+
+    public bool IsInteracting()
+    {
+        return currentlyInteracting;
+    }
+}
---
+++ Assets/scripts/Pickupobject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupobject : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    //HashSet<InteractableItem>();
+
+    private GameObject pickup;
+
+    Vector3 gripPosition;
+
+    // Use this for initialization
+    void Start () {
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "shape") {
+            pickup = collider.gameObject;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider collider){
+        if (collider.tag == "shape")
+            pickup = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (controller == null){
+            Debug.Log("controller not found");
+            return;
+        }
+
+        if (controller.GetPressDown(gripButton) && pickup != null){
+            Debug.Log("Success");
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().useGravity = false;
+
+        }
+        if (controller.GetPressUp(gripButton) && pickup != null){
+            pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = true;
+        }
+       
+    }
+
+}
+
